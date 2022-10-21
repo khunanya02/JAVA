25fd48dc3eb2bcf7576d2d800ff36f17995af9ac
khunanya02 <jah.270245@gmail.com>
2022-10-21 11:07:38 +0700

    JaJaa
    FFFFFF

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -2,5 +2,7 @@ public class Hello{
 
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("My name is JaJaa");
+        System.out.println("5555");
     }
 }
